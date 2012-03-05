a3763c107d050b8c9c161fdc3074ae019f58c6a4
Seth Woodworth <seth@sethish.com>
2012-03-05 01:19:36 -0500

    saves datetime object

--- translate.py
+++ translate.py
@@ -2,13 +2,14 @@
 
 import datetime
 import csv
+import time
 
 import pymongo
 
-def init_mongo(dbname):
+def init_mongo(dbname, collection):
     conn = pymongo.Connection() # put db info here
-    fc = conn[dbname]
-    t = fc.test_collection
+    db = conn[dbname]
+    t = db[collection]
     return t
 
 def read_csv(file):
@@ -32,8 +33,6 @@ def insert_courses(collection, courses):
         collection.insert(c)
 
 def week_parse(courses):
-    dates = [x['meetings'] for x in courses]
-    dates.sort()
     days = {
         'TuTh': [1,3],
          'W': [2],
@@ -51,13 +50,13 @@ def week_parse(courses):
         d = date.split(' ') # split the date after the first or each space
         weekly_classes = days[d[0]]
 
-        time = d[1].split('-') # split the time segment to get the start time
+        ntime = d[1].split('-') # split the time segment to get the start time
 
         # one for each of the days of the week a course is held
         for day in weekly_classes:
             event = course # an event is a weekly instance of a course
             event['weekday'] = day
-            event['time_start'] = time[0] # the starting time of the course
+            event['time_start'] = ntime[0] # the starting time of the course
             weekly_events.append(event)
 
     return weekly_events
@@ -75,20 +74,39 @@ def iso_to_gregorian(iso_year, iso_week, iso_day):
     return year_start + datetime.timedelta(days=iso_day-1, weeks=iso_week-1)
 
 def semester_parse(weekly, t):
-    thing = []
-    for lecture in weekly:
-        weekday = lecture['weekday']
-        for week in range(1,3):
-            thing.append( iso_to_gregorian(2012, week, weekday) )
-    return thing
+    lectures = []
+    for weekly_lecture in weekly:
+        weekday = weekly_lecture['weekday']
+        print weekday
+        # Turn lecture['time_start'] into a time object
+        timey = time.strptime(weekly_lecture['time_start'], '%H:%M%p')
+        # make a datetime.time object to combine later
+        dtime = datetime.time(timey.tm_hour, timey.tm_min)
+        # for week in first week of semester to finals week @ harvard
+        # may 4th is the last possible class date before may 5th finals
+        # http://webdocs.registrar.fas.harvard.edu/general_docs/final_exams.html
+        # > datetime.date(2012, 5, 4).isocalendar() >> (2012, 18, 5)
+        for week in range(1,18):
+            event = {}
+            event['title'] = weekly_lecture['title']
+            event['number'] = weekly_lecture['number']
+            event['field'] = weekly_lecture['field']
+
+            # from the week information, make a date element
+            date = iso_to_gregorian(2012, week, weekday)
+            # save this new datetime object of the class start into lect
+            event['date'] = datetime.datetime.combine(date, dtime)
+            # save lect in a new list of all lecture_events
+            lectures.append(event)
+    return lectures
 
 def main():
     file = './courses_2012_spring.csv'
     courses = read_csv(file)
-    t = init_mongo('fc')
-    #insert_courses(t, courses)
+    t = init_mongo('fc', 'LiveCourses')
     weekly_courses = week_parse(courses)
-    return semester_parse(weekly_courses, t)
+    weekly_lectures = semester_parse(weekly_courses, t)
+    insert_courses(t, weekly_lectures)
 
 if __name__ == '__main__':
     e = main()
